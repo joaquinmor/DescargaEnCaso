4f3d81ed8938a8b814c323d1fab5d0a7f4aa980b
Joaquín Morón <j.moron@outlook.com>
2023-11-20 18:48:33 -0600

    added tabs and starting to create tab 3 controls

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('My Flutter App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(child: Text('Tab 1')),
+          Center(child: Text('Tab 2')),
+          Center(
+              child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              Center(child: Text('Tab 1')),
+              Center(child: Text('Tab 2')),
+              Tab3(),
+            ],
+          )),
+        ],
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.blue,
+        child: TabBar(
+          controller: _tabController,
+          tabs: <Tab>[
+            Tab(icon: Icon(Icons.looks_one)),
+            Tab(icon: Icon(Icons.looks_two)),
+            Tab(icon: Icon(Icons.looks_3)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Tab3 extends StatefulWidget {
+  const Tab3({Key? key}) : super(key: key);
+
+  @override
+  _Tab3State createState() => _Tab3State();
+}
+
+class _Tab3State extends State<Tab3> {
+  DateTime _selectedTime = DateTime.now();
+  bool _checkboxValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Button'),
+              onPressed: () {
+                // Button pressed
+              },
+            ),
+          ),
+          SizedBox(height: 16),
+          TimePickerSpinner(
+            is24HourMode: false,
+            normalTextStyle: TextStyle(fontSize: 24, color: Colors.deepOrange),
+            highlightedTextStyle: TextStyle(fontSize: 24, color: Colors.yellow),
+            spacing: 50,
+            itemHeight: 80,
+            isForce2Digits: true,
+            onTimeChange: (time) {
+              setState(() {
+                _selectedTime = time;
+              });
+            },
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: Text(
+                'Selected time: ${_selectedTime.hour}:${_selectedTime.minute}'),
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: Checkbox(
+              value: _checkboxValue,
+              onChanged: (bool? value) {
+                setState(() {
+                  _checkboxValue = value!;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
